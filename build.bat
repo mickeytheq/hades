8073d7f6f18801d2a42dbe5274a99036e6f80811
mickeytheq <42071167+mickeytheq@users.noreply.github.com>
2025-12-06 22:36:37 +0000

    Rename to hades

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call mvn clean
 call mvn package
-copy .\target\AHLCG4J-1.0-SNAPSHOT.jar c:\Users\micha\AppData\Roaming\StrangeEons3\plug-ins\ahlcg4j.seext
+copy .\target\hades-1.0-SNAPSHOT.jar c:\Users\micha\AppData\Roaming\StrangeEons3\plug-ins\hades-dev.seext
