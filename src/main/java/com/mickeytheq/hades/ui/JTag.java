6a88f26122e550b05476fb16339db68ee75821c3
mickeytheq <42071167+mickeytheq@users.noreply.github.com>
2026-03-30 14:43:00 +0100

    Painting - Disable anti-aliasing for tag text as it improves the look

--- src/main/java/com/mickeytheq/hades/ui/JTag.java
+++ src/main/java/com/mickeytheq/hades/ui/JTag.java
@@ -128,10 +128,6 @@ public class JTag extends JComponent {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
         g2.setColor(getBackground());
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
 
